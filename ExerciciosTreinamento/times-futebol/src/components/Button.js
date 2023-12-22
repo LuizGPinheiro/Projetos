fb2391f4d2f965b96e2e8e72f49fe37756c8e880
LuizGPinheiro <lg.pinheirodev@gmail.com>
2023-12-22 13:31:29 -0300

    Novas mudanças de estilização e estilizando direto no componente.

--- ExerciciosTreinamento/times-futebol/src/components/Button.js
+++ ExerciciosTreinamento/times-futebol/src/components/Button.js
@@ -3,8 +3,17 @@ import React from 'react';
 const Button = ( {team, onClick} ) => {
 
     return (
-            <button onClick={onClick}>
-                Clique aqui para ver Informações do {team}
+            <button 
+            style={{
+                backgroundColor: '#ddd9d8',
+                color: '#000000',
+                padding: '10px 15px',
+                fontSize: '16px',
+                border: 'none',
+                cursor: 'pointer',
+            }}
+                onClick={onClick}>
+                    Clique aqui para ver Informações do {team}
             </button>
     );
 };
